69c57542a42df6f32dec5c25127ff6771bcaad14
legoj <86225902+legoj@users.noreply.github.com>
2022-12-18 22:52:48 +0900

    Add files via upload

---
+++ My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following 
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("synfo")> 
+<Assembly: AssemblyDescription("System Info Dumper")> 
+<Assembly: AssemblyCompany("Citrix Japan RnD")> 
+<Assembly: AssemblyProduct("synfo")> 
+<Assembly: AssemblyCopyright("Jo Gel Santiago 2010")> 
+<Assembly: AssemblyTrademark("")> 
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("be98b848-38cf-4455-a776-2929f8182fcb")> 
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version 
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers 
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")> 
+
+<Assembly: AssemblyVersion("1.0.0.0")> 
+<Assembly: AssemblyFileVersion("1.0.0.0")> 
