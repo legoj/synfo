f9e46c73ea6ac7e36df3075d22559ccf68228a87
legoj <86225902+legoj@users.noreply.github.com>
2025-04-28 19:53:21 +0900

    Update synfo.ps1

--- pssynfo/synfo.ps1
+++ pssynfo/synfo.ps1
@@ -1,43 +1,65 @@
 
 Class Synfo{
-    static [string]$_DEFCFG="PD94bWwgdmVyc2lvbj0iMS4wIiBlbmNvZGluZz0idXRmLTgiID8+CjxzeW5mbz4KCTxkdW1wPgoJCTxjb21waW5mbyB0eXA9Im5mbyIgb2JqPSJuZm8iPgoJCQk8ZmxkPlZlcnNpb24sQ2FwdGlvbixJbnN0YWxsRGF0ZSxMb2NhbGUsT1NBcmNoaXRlY3R1cmUsT1NMYW5ndWFnZSxTeXN0ZW1EaXJlY3RvcnksU3lzdGVtRHJpdmUsVG90YWxWaXNpYmxlTWVtb3J5U2l6ZTwvZmxkPgoJCTwvY29tcGluZm8+CgkJPHNlcnZpY2VzIHR5cD0id21pIiBvYmo9InN2YyI+CgkJCTxjbHM+V2luMzJfU2VydmljZTwvY2xzPgoJCQk8ZmxkPkFjY2VwdFN0b3AsQ2FwdGlvbixDaGVja1BvaW50LENyZWF0aW9uQ2xhc3NOYW1lLERlc2NyaXB0aW9uLERlc2t0b3BJbnRlcmFjdCxEaXNwbGF5TmFtZSxFcnJvckNvbnRyb2wsRXhpdENvZGUsSW5zdGFsbERhdGUsTmFtZSxQYXRoTmFtZSxQcm9jZXNzSWQsU2VydmljZVNwZWNpZmljRXhpdENvZGUsU2VydmljZVR5cGUsU3RhcnRlZCxTdGFydE1vZGUsU3RhcnROYW1lLFN0YXRlLFN0YXR1cyxTeXN0ZW1DcmVhdGlvbkNsYXNzTmFtZSxTeXN0ZW1OYW1lLFRhZ0lkLFdhaXRIaW50PC9mbGQ+CgkJCTxrZXk+TmFtZTwva2V5PgoJCQk8ZXh0IGZsZD0iVmVyc2lvbiI+UGF0aE5hbWU8L2V4dD4KCQk8L3NlcnZpY2VzPgoJCTxob3RmaXhlcyB0eXA9IndtaSIgb2JqPSJxZmUiPgoJCQk8Y2xzPldpbjMyX1F1aWNrRml4RW5naW5lZXJpbmc8L2Nscz4KCQkJPGZsZD5DYXB0aW9uLERlc2NyaXB0aW9uLEhvdEZpeElELEluc3RhbGxlZEJ5LEluc3RhbGxlZE9uPC9mbGQ+CgkJCTxrZXk+SG90Rml4SUQ8L2tleT4KCQk8L2hvdGZpeGVzPgoJCTxkcml2ZXJzIHR5cD0id21pIiBvYmo9ImRydiI+CgkJCTxjbHM+V2luMzJfU3lzdGVtRHJpdmVyPC9jbHM+CgkJCTxmbGQ+QWNjZXB0UGF1c2UsQWNjZXB0U3RvcCxDYXB0aW9uLERlc2NyaXB0aW9uLERlc2t0b3BJbnRlcmFjdCxEaXNwbGF5TmFtZSxFcnJvckNvbnRyb2wsRXhpdENvZGUsTmFtZSxQYXRoTmFtZSxTZXJ2aWNlU3BlY2lmaWNFeGl0Q29kZSxTZXJ2aWNlVHlwZSxTdGFydGVkLFN0YXJ0TW9kZSxTdGFydE5hbWUsU3RhdGUsU3RhdHVzLFRhZ0lkPC9mbGQ+CgkJCTxrZXk+TmFtZTwva2V5PgoJCQk8ZXh0IGZsZD0iVmVyc2lvbiI+UGF0aE5hbWU8L2V4dD4KCQk8L2RyaXZlcnM+CgkJPGFycHJlZ3MgdHlwPSJyZWciIG9iaj0iYXJwIj4KCQkJPHg2ND5TT0ZUV0FSRVxNaWNyb3NvZnRcV2luZG93c1xDdXJyZW50VmVyc2lvblxVbmluc3RhbGxcPC94NjQ+CgkJCTx4ODY+U09GVFdBUkVcV09XNjQzMk5vZGVcTWljcm9zb2Z0XFdpbmRvd3NcQ3VycmVudFZlcnNpb25cVW5pbnN0YWxsXDwveDg2PgoJCTwvYXJwcmVncz4KCQk8cHJvZHVjdHMgdHlwPSJ3aXgiIG9iaj0iYXBwIj4KCQkJPHByZD5Qcm9kdWN0TmFtZSxWZXJzaW9uU3RyaW5nLEluc3RhbGxEYXRlLFB1Ymxpc2hlcixVUkxJbmZvQWJvdXQsSW5zdGFsbExvY2F0aW9uLEluc3RhbGxTb3VyY2UsUGFja2FnZU5hbWU8L3ByZD4KCQkJPHBjaD5EaXNwbGF5TmFtZSxJbnN0YWxsRGF0ZSxNb3JlSW5mb1VSTCxTdGF0ZSxMb2NhbFBhY2thZ2UsVHJhbnNmb3JtcyxVbmluc3RhbGxhYmxlPC9wY2g+CgkJPC9wcm9kdWN0cz4KCTwvZHVtcD4KCTxjb21wPgoJPC9jb21wPgo8L3N5bmZvPg=="
-    [string]$ConfigXml = "$global:PSScriptRoot\synfo.xml"
-    [WCO]$SysProps = $null  #WCO
+    hidden static [string]$_DEFCFG="PD94bWwgdmVyc2lvbj0iMS4wIiBlbmNvZGluZz0idXRmLTgiID8+CjxzeW5mbz4KCTxkdW1wPgoJCTxpbmZvIHR5cD0ibmZvIiBvYmo9Im5mbyI+CgkJCTxmbGQ+VmVyc2lvbixDYXB0aW9uLEluc3RhbGxEYXRlLExvY2FsZSxPU0FyY2hpdGVjdHVyZSxPU0xhbmd1YWdlLFN5c3RlbURpcmVjdG9yeSxTeXN0ZW1Ecml2ZSxUb3RhbFZpc2libGVNZW1vcnlTaXplPC9mbGQ+CgkJPC9pbmZvPgoJCTxzZXJ2aWNlcyB0eXA9IndtaSIgb2JqPSJzdmMiPgoJCQk8Y2xzPldpbjMyX1NlcnZpY2U8L2Nscz4KCQkJPGZsZD5BY2NlcHRTdG9wLENhcHRpb24sQ2hlY2tQb2ludCxDcmVhdGlvbkNsYXNzTmFtZSxEZXNjcmlwdGlvbixEZXNrdG9wSW50ZXJhY3QsRGlzcGxheU5hbWUsRXJyb3JDb250cm9sLEV4aXRDb2RlLEluc3RhbGxEYXRlLE5hbWUsUGF0aE5hbWUsUHJvY2Vzc0lkLFNlcnZpY2VTcGVjaWZpY0V4aXRDb2RlLFNlcnZpY2VUeXBlLFN0YXJ0ZWQsU3RhcnRNb2RlLFN0YXJ0TmFtZSxTdGF0ZSxTdGF0dXMsU3lzdGVtQ3JlYXRpb25DbGFzc05hbWUsU3lzdGVtTmFtZSxUYWdJZCxXYWl0SGludDwvZmxkPgoJCQk8a2V5Pk5hbWU8L2tleT4KCQkJPGV4dCBmbGQ9IlZlcnNpb24iPlBhdGhOYW1lPC9leHQ+CgkJPC9zZXJ2aWNlcz4KCQk8aG90Zml4ZXMgdHlwPSJ3bWkiIG9iaj0icWZlIj4KCQkJPGNscz5XaW4zMl9RdWlja0ZpeEVuZ2luZWVyaW5nPC9jbHM+CgkJCTxmbGQ+Q2FwdGlvbixEZXNjcmlwdGlvbixIb3RGaXhJRCxJbnN0YWxsZWRCeSxJbnN0YWxsZWRPbjwvZmxkPgoJCQk8a2V5PkhvdEZpeElEPC9rZXk+CgkJPC9ob3RmaXhlcz4KCQk8ZHJpdmVycyB0eXA9IndtaSIgb2JqPSJkcnYiPgoJCQk8Y2xzPldpbjMyX1N5c3RlbURyaXZlcjwvY2xzPgoJCQk8ZmxkPkFjY2VwdFBhdXNlLEFjY2VwdFN0b3AsQ2FwdGlvbixEZXNjcmlwdGlvbixEZXNrdG9wSW50ZXJhY3QsRGlzcGxheU5hbWUsRXJyb3JDb250cm9sLEV4aXRDb2RlLE5hbWUsUGF0aE5hbWUsU2VydmljZVNwZWNpZmljRXhpdENvZGUsU2VydmljZVR5cGUsU3RhcnRlZCxTdGFydE1vZGUsU3RhcnROYW1lLFN0YXRlLFN0YXR1cyxUYWdJZDwvZmxkPgoJCQk8a2V5Pk5hbWU8L2tleT4KCQkJPGV4dCBmbGQ9IlZlcnNpb24iPlBhdGhOYW1lPC9leHQ+CgkJPC9kcml2ZXJzPgoJCTxhcnByZWdzIHR5cD0icmVnIiBvYmo9ImFycCI+CgkJCTx4NjQ+U09GVFdBUkVcTWljcm9zb2Z0XFdpbmRvd3NcQ3VycmVudFZlcnNpb25cVW5pbnN0YWxsXDwveDY0PgoJCQk8eDg2PlNPRlRXQVJFXFdPVzY0MzJOb2RlXE1pY3Jvc29mdFxXaW5kb3dzXEN1cnJlbnRWZXJzaW9uXFVuaW5zdGFsbFw8L3g4Nj4KCQk8L2FycHJlZ3M+CgkJPHByb2R1Y3RzIHR5cD0id2l4IiBvYmo9ImFwcCI+CgkJCTxwcmQ+UHJvZHVjdE5hbWUsVmVyc2lvblN0cmluZyxJbnN0YWxsRGF0ZSxQdWJsaXNoZXIsVVJMSW5mb0Fib3V0LEluc3RhbGxMb2NhdGlvbixJbnN0YWxsU291cmNlLFBhY2thZ2VOYW1lPC9wcmQ+CgkJCTxwY2g+RGlzcGxheU5hbWUsSW5zdGFsbERhdGUsTW9yZUluZm9VUkwsU3RhdGUsTG9jYWxQYWNrYWdlLFRyYW5zZm9ybXMsVW5pbnN0YWxsYWJsZTwvcGNoPgoJCTwvcHJvZHVjdHM+Cgk8L2R1bXA+Cjwvc3luZm8+"
+    hidden static [string]$ConfigXml = "$global:PSScriptRoot\synfo.xml"
+    [WCO]$SysProps = $null  #WCO info
     $WCObjects = @{}   #WCC coll
-    [void]DumpObjects(){
-        if(!(Test-Path $this.ConfigXml)){
-            [Text.Encoding]::Utf8.GetString([Convert]::FromBase64String([Synfo]::_DEFCFG)) | Out-File $this.ConfigXml -Encoding utf8
-        }
-        [xml]$xml = gc $this.ConfigXml
+    [string]$xmlPath = "$PSScriptRoot\synfo_$($env:COMPUTERNAME)_$((Get-Date).ToString("yyyymmddHHMMss")).xml"
+    Synfo(){
+        if(!(Test-Path $([Synfo]::ConfigXml))){   [Text.Encoding]::Utf8.GetString([Convert]::FromBase64String([Synfo]::_DEFCFG)) | Out-File $([Synfo]::ConfigXml) -Encoding utf8  }
+        [xml]$xml = gc $([Synfo]::ConfigXml)
         foreach($child in $xml.DocumentElement.dump.ChildNodes){
             [System.Xml.XmlElement]$ch = $child
             $typ = $ch.GetAttribute("typ")
             switch($typ){
-                'wmi'{ $this.ReadWMIObjects($ch)  }
-                'reg'{ $this.ReadRegObjects($ch)  }
-                'wix'{ $this.ReadWIXObjects($ch)  }
-                'nfo'{ $this.ReadComputerInfo($ch)}
+                'wmi'{ $wcc = [Synfo]::ReadWMIObjects($ch); $this.WCObjects.Add($wcc.Name,$wcc)  }
+                'reg'{ $wcc = [Synfo]::ReadRegObjects($ch); $this.WCObjects.Add($wcc.Name,$wcc)  }
+                'wix'{ $wcc = [Synfo]::ReadWIXObjects($ch); $this.WCObjects.Add($wcc.Name,$wcc)  }
+                'nfo'{ $this.SysProps = [Synfo]::ReadComputerInfo($ch)}
                 default{}
             }
         }
     }
-    [void]ReadComputerInfo([System.Xml.XmlElement]$xml){
+    Synfo([string]$synfoXml){
+        [xml]$xml = gc $synfoXml
+        foreach($child in $xml.DocumentElement.ChildNodes){
+            [System.Xml.XmlElement]$ch = $child
+            if($ch.HasAttribute("count")){    $wc = [WCC]::ParseXml($ch); $this.WCObjects.Add($wc.Name,$wc) }
+            elseif($ch.LocalName -eq "info"){ $this.SysProps = [WCO]::ParseXml($ch) }
+        }
+        $this.xmlPath = $synfoXml    
+    }
+    [string]Save(){
+        $w = [System.Xml.XmlTextWriter]::new($this.xmlPath, [Text.Encoding]::UTF8)
+        $w.Formatting = [System.Xml.Formatting]::Indented
+        $w.WriteStartDocument()
+        $w.WriteStartElement("synfo")
+        $this.SysProps.WriteXml($w)
+        foreach($wco in $this.WCObjects.Values){ $wco.WriteXml($w) }
+        $w.WriteEndElement()
+        $w.WriteEndDocument()
+        $w.Close()
+        return $this.xmlPath
+    }
+    [string]Compare([string]$refXml){ return [Synfo]::Compare([Synfo]::new($refXml),$this)  }
+
+    hidden static [WCO]ReadComputerInfo([System.Xml.XmlElement]$xml){
         Write-Host "Dumping System Info"
-        $this.SysProps = [WCO]::new($xml.LocalName,$env:COMPUTERNAME)
+        $wcoSys = [WCO]::new($xml.LocalName,$env:COMPUTERNAME)
 
         $fld = $xml.SelectSingleNode("fld").InnerText.Split(",")
         Write-Host "AddProps from WMI.OS"
         $nfo = gwmi -ClassName Win32_OperatingSystem -Property $fld 
-        foreach($p in $nfo.Properties){ $this.SysProps.AddProp($p.Name,$p.Value)  }
+        foreach($p in $nfo.Properties){ $wcoSys.AddProp($p.Name,$p.Value)  }
 
         $wen = [WCO]::new("envvars","SYSTEM")
-        $this.SysProps.SubProps = $wen
+        $wcoSys.SubProps = $wen
         $sev = [Environment]::GetEnvironmentVariables(2)
         Write-Host "AddProps from ENV.Vars - SYSTEM"
         foreach($p in $sev.Keys){ $wen.AddProp($p,$sev[$p])    }        
-        
+        return $wcoSys
     }
-    [void]ReadWIXObjects([System.Xml.XmlElement]$xml){
+    hidden static [WCC]ReadWIXObjects([System.Xml.XmlElement]$xml){
         $obj = $xml.GetAttribute("obj")
         $nam = $xml.LocalName
         $prd = $xml.SelectSingleNode("prd").InnerText.Split(",")
@@ -69,10 +91,9 @@ Class Synfo{
             }
             $wcc.Members.Add($wco.Id,$wco)
         }
-        $this.WCObjects.Add($wcc.Name,$wcc)
-        
+        return $wcc
     }
-    [void]ReadRegObjects([System.Xml.XmlElement]$xml){
+    hidden static [WCC]ReadRegObjects([System.Xml.XmlElement]$xml){
         $obj = $xml.GetAttribute("obj")
         $nam = $xml.LocalName        
         $wcc = [WCC]::new($nam)
@@ -87,9 +108,9 @@ Class Synfo{
                 if(!$wcc.Members.ContainsKey($wco.Id)){  $wcc.Members.Add($wco.Id,$wco) }
             }
         }
-        $this.WCObjects.Add($wcc.Name,$wcc)
+        return $wcc
     }
-    [void]ReadWMIObjects([System.Xml.XmlElement]$xml){
+    hidden static [WCC]ReadWMIObjects([System.Xml.XmlElement]$xml){
         $obj = $xml.GetAttribute("obj")
         $nam = $xml.LocalName
         $cls = $xml.SelectSingleNode("cls").InnerText
@@ -115,70 +136,10 @@ Class Synfo{
                 }
             }
             $wcc.Members.Add($wco.Id,$wco)
-
         }
-        $this.WCObjects.Add($wcc.Name,$wcc)
+        return $wcc
     }
-    [void]ReadXML([string]$synfoXml){
-        [xml]$xml = gc $synfoXml
-        foreach($child in $xml.DocumentElement.ChildNodes){
-            [System.Xml.XmlElement]$ch = $child
-            if($ch.HasAttribute("count")){
-                $wc = [WCC]::ParseXml($ch)
-                $this.WCObjects.Add($wc.Name,$wc)
-            }elseif($ch.HasAttribute("id")){
-                $this.SysProps = [WCO]::ParseXml($ch)
-            }
-        }
-    }
-    [void]SaveToXML([string]$outXmlPath){
-        $w = [System.Xml.XmlTextWriter]::new($outXmlPath, [Text.Encoding]::UTF8)
-        $w.Formatting = [System.Xml.Formatting]::Indented
-        $w.WriteStartDocument()
-        $w.WriteStartElement("synfo")
-        $this.SysProps.WriteXml($w)
-        foreach($wco in $this.WCObjects.Values){ $wco.WriteXml($w) }
-        $w.WriteEndElement()
-        $w.WriteEndDocument()
-        $w.Close()
-    }
-    [string]CurrentMethod () {
-        return (Get-PSCallStack)[1].FunctionName
-    }
-    static [void]CompareXML([string]$refXml,[string]$cmpXml,[string]$resXml){
-        $refSyn = [Synfo]::new(); $refSyn.ReadXML($refXml)
-        $cmpSyn = [Synfo]::new(); $cmpSyn.ReadXML($cmpXml)
-
-        $w = [System.Xml.XmlTextWriter]::new($resXml, [Text.Encoding]::UTF8)
-        $w.Formatting = [System.Xml.Formatting]::Indented
-        $w.WriteStartDocument()
-        $w.WriteStartElement("synfo")
-        $w.WriteStartElement("ref")
-        $w.WriteAttributeString("path",$refXml)
-        $w.WriteEndElement()
-        $w.WriteStartElement("cmp")
-        $w.WriteAttributeString("path",$cmpXml)
-        $w.WriteEndElement()
-        $w.WriteStartElement("result")
-        $wcoRes = [CRO]::Compare($refSyn.SysProps,$cmpSyn.SysProps)        
-        $w.WriteAttributeString("hasChanges",$wcoRes.HasChanges())
-        $wcoRes.WriteXml($w)
-        foreach($wcc in $refSyn.WCObjects.Values){            
-            $wccRes = [CRC]::Compare($wcc,$cmpSyn.WCObjects[$wcc.Name])
-            $wccRes.WriteXml($w)
-        }
-        $w.WriteEndElement() #result
-        $w.WriteEndElement()
-        $w.WriteEndDocument()
-        $w.Close()       
-
-        #
-        #$wcoRes.WriteXml()
-        #foreach($wcc in $other.WCObjects.Values){
-        #    $wcc.Compare($other.WCObjects[$wcc.Name])
-        #}
-    }
-    static [string]ToStringValue($pVal){
+    hidden static [string]ToStringValue($pVal){
         $p = ""
         if($pVal -eq $null){ return $p }
         $t = $pVal.GetType()
@@ -189,6 +150,39 @@ Class Synfo{
             default{ $p = "[$t]"}
         }
         return $p
+    }
+
+    static [string]Dump(){ return ([Synfo]::new()).Save() }
+    static [string]Compare([string]$refXml,[string]$cmpXml){ return [Synfo]::Compare([Synfo]::new($refXml),[Synfo]::new($cmpXml)) }
+    static [string]Compare([Synfo]$refSynfo,[Synfo]$cmpSynfo){
+        $cMode = "xcmp"
+        if($refSynfo.SysProps.Id -eq $cmpSynfo.SysProps.Id){ $cMode = "diff_$($refSynfo.SysProps.Id)-$($cmpSynfo.SysProps.Id)" }
+        else{ $cMode = "xcmp_$($refSynfo.SysProps.Id)-$($cmpSynfo.SysProps.Id)" }
+
+        $resXml = "$PSScriptRoot\synfo_$($cMode)_$((Get-Date).ToString("yyyymmddHHMMss")).xml"
+        $w = [System.Xml.XmlTextWriter]::new($resXml, [Text.Encoding]::UTF8)
+        $w.Formatting = [System.Xml.Formatting]::Indented
+        $w.WriteStartDocument()
+        $w.WriteStartElement("synfo")
+        $w.WriteStartElement("ref")
+        $w.WriteAttributeString("path",$refSynfo.xmlPath)
+        $w.WriteEndElement()
+        $w.WriteStartElement("cmp")
+        $w.WriteAttributeString("path",$cmpSynfo.xmlPath)
+        $w.WriteEndElement()
+        $w.WriteStartElement("result")
+        $wcoRes = [CRO]::Compare($refSynfo.SysProps,$cmpSynfo.SysProps)        
+        $w.WriteAttributeString("hasChanges",$wcoRes.HasChanges())
+        $wcoRes.WriteXml($w)
+        foreach($wcc in $refSynfo.WCObjects.Values){            
+            $wccRes = [CRC]::Compare($wcc,$cmpSynfo.WCObjects[$wcc.Name])
+            $wccRes.WriteXml($w)
+        }
+        $w.WriteEndElement() #result
+        $w.WriteEndElement()
+        $w.WriteEndDocument()
+        $w.Close()       
+        return $resXml
     }
 
 }
@@ -474,18 +468,10 @@ Class CRC:CR{
     }
 }
 
-[Synfo]::CompareXML("$PSScriptRoot\sample1.xml","$PSScriptRoot\sample2.xml","$PSScriptRoot\testfile.xml")
 
-#$syn = [Synfo]::new()
-#$syn.DumpObjects()
-#$syn.SaveToXML("$PSScriptRoot\testsave.xml")
+#sample usage
+#[Synfo]::Dump()
 
-#$syn.ReadXML("$PSScriptRoot\sample1.xml")
+[Synfo]::Compare("$PSScriptRoot\sample1.xml","$PSScriptRoot\sample2.xml")
 
-#$syn2 = [Synfo]::new()
-#$syn2.ReadXML("$PSScriptRoot\sample2.xml")
 
-#$syn.Compare($syn2)
-
-#$syn.DumpObjects()
-#$syn.SaveToXML("$PSScriptRoot\testsave.xml")
